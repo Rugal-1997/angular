9e8d3e896f15f520a98b5ea6843334f3bf97d41d
Nicolas GM <nicolasgomez.andap@gmail.com>
2018-10-16 17:04:31 -0500

    Angular - Typescript

---
+++ src/app/usuarios.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';  /* Inyectar servicio */
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+	url:string;
+	encabezados:any; 
+
+	constructor(private http:HttpClient) {
+		this.url="https://apidocumentospiensadigital.herokuapp.com";
+		this.encabezados={ headers: new HttpHeaders({
+			'Content-Type': 'Application/json' })
+		}
+	}
+
+	iniciarSesion(usuario:any):Observable<any>{
+		return this.http.post<any>(this.url+'/user_token',usuario,this.encabezados);
+	}
+
+	crearCuenta(usuario:any):Observable<any>{
+		return this.http.post<any>(this.url+'/users',usuario,this.encabezados);
+	} 
+}
